18e77991f5a2b4b469f927e7c7af3edfe15758fb
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2023-12-28 21:28:40 +0500

    Create main components (empty for now)

---
+++ src/components/Portfolio.jsx
@@ -0,0 +1,12 @@
+import Header from './Header';
+import Footer from './Footer';
+
+const Portfolio = () => (
+  <>
+    <Header />
+    <p>PORTFOLIO</p>
+    <Footer />
+  </>
+);
+
+export default Portfolio;
