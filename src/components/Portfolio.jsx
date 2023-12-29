af88a04f4a3a0f24be35263fd4e3cc86a20592ed
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2023-12-30 02:37:39 +0500

    Update Portfolio component (render Hero and Projects components)

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,10 +1,15 @@
+// import { Outlet } from 'react-router-dom';
 import Header from './Header';
 import Footer from './Footer';
+import Projects from './Projects';
+import Hero from './Hero';
 
 const Portfolio = () => (
   <>
     <Header />
     <p>PORTFOLIO</p>
+    <Projects />
+    <Hero />
     <Footer />
   </>
 );
