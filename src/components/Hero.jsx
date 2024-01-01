fca5d834b5e702e8b2bfda7af351fea741929cc8
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2024-01-01 20:09:41 +0500

    Install react-icons (npm install react-icons --save), add social-media icons, check if they are displayed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,17 @@
+import { FaGithub } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
+import { FaMediumM } from "react-icons/fa";
+import { SiWellfound } from "react-icons/si";
+
 const Hero = () => (
   <section>
     <p>HERO</p>
+    <FaGithub />
+    <FaLinkedin />
+    <FaTwitter />
+    <SiWellfound />
+    <FaMediumM />
   </section>
 );
 
