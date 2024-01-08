a1297894085836d40cf7c30068c6119239289afe
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2024-01-09 02:37:43 +0500

    Apply modular css classes to Project component, write css rules in Project.module.css to style Project element in the Left container

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -2,10 +2,11 @@ import { v4 as uuidv4 } from 'uuid';
 import PropTypes from 'prop-types';
 // import { objectOf } from 'prop-types';
 
-import styles from '../styles/Projects.module.css';
+import styles from '../styles/Project.module.css';
 
 const Project = ({ project }) => {
   const liveDemo = project.live_demo;
+  const image = project.featured_image;
   const {
     name,
     description,
@@ -18,16 +19,16 @@ const Project = ({ project }) => {
       <div className={styles.mockup}>
         Mockup
       </div>
-      <h2 className={styles.white}>{name}</h2>
-      <p>{description}</p>
-      <ul>
+      <h2 className={styles.title}>{name}</h2>
+      <p className={styles.description}>{description}</p>
+      <ul className={styles.tech}>
         {technologies.map((tech) => (
           <li key={uuidv4()}>{tech}</li>
         ))}
       </ul>
-      <div>
-        <a href={source} className={`${styles.link} btn`}>Source Code</a>
-        <a href={liveDemo} className={`${styles.link} btn`}>Live Demo</a>
+      <div className={`${styles.link}`}>
+        <a href={source}>Source Code</a>
+        <a href={liveDemo}>Live Demo</a>
       </div>
     </>
   );
