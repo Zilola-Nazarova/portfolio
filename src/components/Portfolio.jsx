f4635a95b8db1343e8f508e9d8e1fb19fd5e8e6b
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2024-01-13 17:47:03 +0500

    Create doBlink function that toggles blink state, pass the function to both Left and Right components because I want to add an event listener to the whole page

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -4,11 +4,19 @@ import Right from './Right';
 
 const Portfolio = () => {
   const [color, setColor] = useState(0);
+  const [blink, setBlink ] = useState(false);
+  
+  const doBlink = () => {
+    setBlink(prevMode => !prevMode);
+    setTimeout(() =>
+      {setBlink(prevMode => !prevMode)},
+    200)
+  };
 
   return (
     <>
-      <Left colorTheme={(i) => setColor(i)} />
-      <Right color={color} />
+      <Left colorTheme={(i) => setColor(i)} doBlink={() => doBlink()}/>
+      <Right color={color} doBlink={() => doBlink()} blink={blink} />
     </>
   );
 };
