b683fc243021f71d035fda8f5b163383ec6b5abf
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2024-01-17 05:00:13 +0500

    Change icon and title of the index.html to customize the browser tab, fix the links in project.jsx to make them supported by gh-pages

--- src/assets/projects.js
+++ src/assets/projects.js
@@ -2,8 +2,8 @@ const projectDetails = [
   {
     name: 'Blog App',
     description: 'A blog website shows the list of posts and empowers readers to interact with them by adding comments and liking posts. Users need to log in/sign up to write posts and interact with posts of other users. Unregistered users can only read posts.',
-    featured_image: '../../public/blog_app.jpg',
-    alternative_images: ['../../public/blog_app.jpg'],
+    featured_image: './blog_app.jpg',
+    alternative_images: ['./blog_app.jpg'],
     technologies: ['HTML5', 'CSS3', 'Ruby on Rails', 'Devise', 'CanCanCan'],
     live_demo: 'https://blog-app-zilola-3e304db4a389.herokuapp.com/',
     source: 'https://github.com/Zilola-Nazarova/blog-app',
@@ -11,8 +11,8 @@ const projectDetails = [
   {
     name: 'Mountains Forecast',
     description: 'This app lists mountains located in Uzbekistan with small icons representing the current weather code on a peak. Users can search peaks by name. They can navigate to the page with detailed forecast data by clicking on an item.',
-    featured_image: '../../public/mountains_1.jpg',
-    alternative_images: ['../../public/mountains_1.jpg', '../../public/mountains_2.jpg'],
+    featured_image: './mountains_1.jpg',
+    alternative_images: ['./mountains_1.jpg', './mountains_2.jpg'],
     technologies: ['HTML5', 'CSS3', 'JavaScript', 'React', 'Redux'],
     live_demo: 'https://mountains-forecast.onrender.com/',
     source: 'https://github.com/Zilola-Nazarova/mountains-forecast',
@@ -20,8 +20,8 @@ const projectDetails = [
   {
     name: 'Bookstore',
     description: 'This single-page application displays the list of books. Users can add a new book by providing the title, author and selecting a category. They can also remove any  book form the list.',
-    featured_image: '../../public/bookstore.jpg',
-    alternative_images: ['../../public/bookstore.jpg'],
+    featured_image: './bookstore.jpg',
+    alternative_images: ['./bookstore.jpg'],
     technologies: ['HTML5', 'CSS3', 'JavaScript', 'React', 'Redux'],
     live_demo: 'https://bookstore-4xzu.onrender.com/',
     source: 'https://github.com/Zilola-Nazarova/bookstore',
@@ -29,8 +29,8 @@ const projectDetails = [
   {
     name: 'Smart Pocket',
     description: "The SmartPocket app keeps track of users' expences. Users can create, edit, delete categories and transactions. Each transaction is associated with one or many categories. The app sums transactions to show the amount of money spent per category.",
-    featured_image: '../../public/smart_pocket_1.jpg',
-    alternative_images: ['../../public/smart_pocket_1.jpg', '../../public/smart_pocket_2.jpg'],
+    featured_image: './smart_pocket_1.jpg',
+    alternative_images: ['./smart_pocket_1.jpg', './smart_pocket_2.jpg'],
     technologies: ['HTML5', 'CSS3', 'Ruby on Rails', 'Devise', 'CanCanCan'],
     live_demo: 'https://smart-pocket-app.onrender.com/',
     source: 'https://github.com/Zilola-Nazarova/budget-app',
